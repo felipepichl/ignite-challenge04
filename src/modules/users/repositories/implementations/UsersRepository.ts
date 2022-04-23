ac7467c67033cbb4471fc2d76064408fe0dba444
Felipe Pichl <felipe.pichl@hotmail.com>
2022-04-22 23:15:58 -0300

    UsersRepository: findByEmail

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -33,7 +33,7 @@ class UsersRepository implements IUsersRepository {
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.email === email);
   }
 
   turnAdmin(receivedUser: User): User {
