4195ffc553af55e9fd4ecd44fde2e0af57a566da
Felipe Pichl <felipe.pichl@hotmail.com>
2022-04-22 23:13:46 -0300

    UsersRepository: create

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,7 +19,13 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, { name, email, created_at: new Date() });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
