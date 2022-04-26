29a3ad6c37c952e713fdd8af1f21546c18793e4e
Felipe Pichl <felipe.pichl@hotmail.com>
2022-04-25 21:34:29 -0300

    Test: Routes, PATCH/users

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,11 +6,15 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const user_id = request.headers.authorization;
+    const { user_id } = request.params;
 
-    this.turnUserAdminUseCase.execute({ user_id });
+    try {
+      const user = this.turnUserAdminUseCase.execute({ user_id });
 
-    return response.status(200).send();
+      return response.json(user);
+    } catch (err) {
+      return response.status(404).json({ error: err.message });
+    }
   }
 }
 
